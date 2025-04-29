42111937f370dd56458b12737874a1cf42964180
Almaz Galimov <almazwork@gmail.com>
2025-04-29 18:50:46 +0500

    Add create handler

--- internal/delivery/http/article_handler.go
+++ internal/delivery/http/article_handler.go
@@ -2,7 +2,9 @@ package http
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/soulstalker/pantrygo2/internal/entity"
 	"github.com/soulstalker/pantrygo2/internal/usecase"
+	"net/http"
 )
 
 // ArticleHandler обрабатывает HTTP-запросы для работы со статьями
@@ -10,16 +12,55 @@ type ArticleHandler struct {
 	articleUseCase *usecase.ArticleUseCase
 }
 
-func (h *ArticleHandler) RegisterRoutes(r *gin.Engine) {
-	articles := r.Group("/api/articles")
-	articles.POST("/", h.CreateArticle)
-	articles.GET("/:id", h.GetArticle)
-	articles.POST("/:id", h.UpdateArticle)
-	articles.DELETE("/:id", h.DeleteArticle)
+// NewArticleHandler создает новый ArticleHandler
+func NewArticleHandler(articleUseCase *usecase.ArticleUseCase) *ArticleHandler {
+	return &ArticleHandler{
+		articleUseCase: articleUseCase,
+	}
 }
 
+// RegisterRoutes регистрирует маршруты API
+func (h *ArticleHandler) RegisterRoutes(r *gin.Engine) {
+	group := r.Group("/api/group")
+	{
+		group.POST("/", h.CreateArticle)
+		group.GET("/:id", h.GetArticle)
+		group.PUT("/:id", h.UpdateArticle)
+		group.DELETE("/:id", h.ArchiveArticle)
+		group.GET("/", h.ListArticles)
+		group.GET("/search", h.SearchArticles)
+	}
+}
+
+// Структура для запроса на создание статьи
+type articleRequest struct {
+	Title   string   `json:"title" binding:"required"`
+	Content string   `json:"content" binding:"required"`
+	Status  string   `json:"status"`
+	Tags    []string `json:"tags"`
+}
+
+// CreateArticle обрабатывает создание статьи
 func (h *ArticleHandler) CreateArticle(c *gin.Context) {
-	// create
+	var req articleRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	article := &entity.Article{
+		Title:       req.Title,
+		Content:     req.Content,
+		Status:      entity.ArticleStatus(req.Status),
+		CreatedByID: 1, // Мок, потом будет тот кто залогинен
+	}
+
+	if err := h.articleUseCase.Create(c.Request.Context(), article, req.Tags); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"id": article.ID})
 }
 
 func (h *ArticleHandler) GetArticle(c *gin.Context) {
@@ -30,6 +71,14 @@ func (h *ArticleHandler) UpdateArticle(c *gin.Context) {
 	// update
 }
 
-func (h *ArticleHandler) DeleteArticle(c *gin.Context) {
+func (h *ArticleHandler) ArchiveArticle(c *gin.Context) {
+	// delete
+}
+
+func (h *ArticleHandler) ListArticles(c *gin.Context) {
+	// delete
+
+}
+func (h *ArticleHandler) SearchArticles(c *gin.Context) {
 	// delete
 }
